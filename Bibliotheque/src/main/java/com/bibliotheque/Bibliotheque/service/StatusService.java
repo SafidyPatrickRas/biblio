27c5ca24e2d2aa5ed8a1e1770d75c60285b11b49
SafidyPatrickRas <safidypatrickrasolofoniaina@gmail.com>
2025-07-04 05:17:53 +0300

    status

---
+++ Bibliotheque/src/main/java/com/bibliotheque/Bibliotheque/service/StatusService.java
@@ -0,0 +1,32 @@
+package com.bibliotheque.Bibliotheque.service;
+
+import com.bibliotheque.Bibliotheque.model.Status;
+import com.bibliotheque.Bibliotheque.repository.StatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StatusService {
+
+    @Autowired
+    private StatusRepository statusRepository;
+
+    public List<Status> findAll() {
+        return statusRepository.findAll();
+    }
+
+    public Optional<Status> findById(Integer id) {
+        return statusRepository.findById(id);
+    }
+
+    public Status save(Status status) {
+        return statusRepository.save(status);
+    }
+
+    public void deleteById(Integer id) {
+        statusRepository.deleteById(id);
+    }
+}
